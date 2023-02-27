3601f2a2d8f4f5084ca4f189f194dfecd86ae5d0
Shrestha, Manish <Manish.Shrestha@epsilon.com>
2023-02-27 12:39:30 -0600

    Added Nodemon and default port

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 const app = express()
-const port = 3000
+const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World Test!')
 })
 
 app.listen(port, () => {
